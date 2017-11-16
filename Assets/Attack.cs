1527200bac19d64eef0600509739dc1e1073d19e
liam <liam_devlin@live.co.uk>
2017-11-16 18:07:35 +0000

    First Commit
    Basic Outline Created

---
+++ Assets/Attack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+	private float damage;
+
+	public ParticleSystem attack01;
+
+	// Use this for initialization
+	void Start () {
+		damage = 1f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetButtonDown("Fire1")){
+			PlayAttack ();
+			damage = damage;
+		}
+	}
+
+	void PlayAttack(){
+		if (attack01.isPlaying) {
+			
+		} else {
+			attack01.Play ();
+		}
+
+	}
+
+	void DoDamage(){
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speedMultiplier;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetButton("Vertical")){
+			Vector3 pfv = transform.forward;
+			transform.position += pfv * Input.GetAxis("Vertical") * speedMultiplier * Time.deltaTime;
+		}
+		if(Input.GetButton("Horizontal")){
+			Vector3 pfv = transform.right;
+			transform.position += pfv * Input.GetAxis("Horizontal") * speedMultiplier * Time.deltaTime;
+		}
+	}
+}
